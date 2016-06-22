56df09c68e8e2de31b5349e1a5d57b8393d18b8b
Deft_MKJing <714831204@qq.com>
2016-06-22 09:46:39 +0800

    Initial Commit

---
+++ CMPopTip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CMPopTip
+//
+//  Created by 宓珂璟 on 16/6/22.
+//  Copyright © 2016年 宓珂璟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CMPopTip/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CMPopTip
+//
+//  Created by 宓珂璟 on 16/6/22.
+//  Copyright © 2016年 宓珂璟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
